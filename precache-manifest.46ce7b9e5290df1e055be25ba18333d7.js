74b007ba1eb5e46eb176cb26797dfab682bd7a10
Murillo Kajihara <murillo.kajihara@banky.com.br>
2019-03-16 18:03:40 -0300

    Updates

---
+++ precache-manifest.46ce7b9e5290df1e055be25ba18333d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d91fab5a5fc549bfc207c0bbe9cf6ad",
+    "url": "/register/static/media/logobanky.6d91fab5.svg"
+  },
+  {
+    "revision": "b5f2f71c98cc262cf57b",
+    "url": "/register/static/js/runtime~main.13d1ebc8.js"
+  },
+  {
+    "revision": "de052a40ac6e214b2d98",
+    "url": "/register/static/js/main.ae7090d5.chunk.js"
+  },
+  {
+    "revision": "25a661165f8f821fbab7",
+    "url": "/register/static/js/2.7dcdf71c.chunk.js"
+  },
+  {
+    "revision": "de052a40ac6e214b2d98",
+    "url": "/register/static/css/main.2dfac684.chunk.css"
+  },
+  {
+    "revision": "5ba238aa5bff980b51e5ace99ca2400e",
+    "url": "/register/index.html"
+  }
+];
